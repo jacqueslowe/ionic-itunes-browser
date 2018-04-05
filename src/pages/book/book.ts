90866baa7d58d9b3eac48e45ff0b23d31364b839
LUA\jacques <jacques.lowe@gmail.com>
2018-04-05 18:09:24 -0500

    donut, bar

--- src/pages/book/book.ts
+++ src/pages/book/book.ts
@@ -8,6 +8,25 @@ import { LoadingController } from 'ionic-angular';
   templateUrl: './book.html'
 })
 export class BookPage {
+  //donut
+  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
+  public doughnutChartData:number[] = [350, 450, 100];
+  public doughnutChartType:string = 'doughnut';
+
+  //barChart
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+ 
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
   // lineChart
   public lineChartData:Array<any> = [
     {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
